baf926981231e6be0c78e18322c555ccef129832
Yeison Hernandez <developer.ss.2@tucash.co>
2023-05-22 01:47:53 -0500

    Se muestra la información para el tab Qualifications

---
+++ app/job-details/[id].js
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { Stack, useRouter, useSearchParams } from "expo-router";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+import useFetch from "../../hook/useFetch";
+import { COLORS, SIZES, icons } from "../../constants";
+import ScreenHeaderBtn from "../../components/common/header/ScreenHeaderBtn";
+import { Company, JobTabs, Specifics } from "../../components";
+
+const tabs = ["About", "Qualifications", "Responsabilites"];
+
+const JobDetails = () => {
+  const [refreshing, setRefreshing] = useState(false);
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+
+  const onRefetch = () => {};
+
+  const displayTabContent = () => {
+    switch (activeTab) {
+      case "Qualifications":
+        return (
+          <Specifics
+            title="Qualifications"
+            points={data[0].job_highlights?.Qualifications ?? ["N/A"]}
+          />
+        );
+      case "About":
+      case "Responsabilites":
+
+      default:
+        break;
+    }
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerBackVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.left}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn
+              iconUrl={icons.share}
+              dimension="60%"
+              handlePress={() => router.back()}
+            />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefetch} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].company_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+              <JobTabs
+                tabs={tabs}
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+
+              {displayTabContent()}
+            </View>
+          )}
+        </ScrollView>
+      </>
+    </SafeAreaView>
+  );
+};
+
+export default JobDetails;
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,25 +1,45 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, Image } from "react-native";
 
-import styles from './company.style'
-import { Stack, useRouter } from 'expo-router'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { COLORS } from '../../../constants'
+import styles from "./company.style";
+import { Stack, useRouter } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS, icons } from "../../../constants";
+import { checkImageURL } from "../../../utils";
 
-const Company = () => {
+const Company = ({ companyLogo, jobTitle, companyName, location }) => {
   const router = useRouter();
 
   return (
-    <SafeAreaView
-    style={{flex: 1, backgroundColor: COLORS.lightWhite}}
-    >
-      <Stack.Screen 
-        options={{
-          headerStyle: {backgroundColor: COLORS.lightWhite}
-        }}
-      />
-    </SafeAreaView>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+          style={styles.logoImage}
+        />
+      </View>
 
-export default Company
+      <View style={styles.jobTitleBox}>
+        <Text style={styles.jobTitle}>{jobTitle}</Text>
+      </View>
+
+      <View style={styles.companyInfoBox}>
+        <Text style={styles.companyName}>{companyName} / </Text>
+        <View style={styles.locationBox}>
+          <Image
+            source={icons.location}
+            resizeMode="contain"
+            style={styles.locationImage}
+          />
+          <Text style={styles.locationName}>{location}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Company;
